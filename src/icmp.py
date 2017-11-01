74ba4737a418856bcda1d5c2b4dd82b1aa8fbf27
Taylan Dogan <kondanta@users.noreply.github.com>
2017-11-01 14:02:04 +0300

    The initial implementation of port identification is added. It finds port numbers, service types and their statuses. However currently It does not write them into a text file.

--- src/icmp.py
+++ src/icmp.py
@@ -1,4 +1,7 @@
 from scapy.all import *
+import socket
+import os
+import re
 
 
 conf.L3socket = L3RawSocket
@@ -18,7 +21,7 @@ def icmp_ping(ip1='192.168.1.1', ip2='192.168.1.255'):
 			reply = sr1(packet, timeout=TIMEOUT)
 			if not (reply is None):
 				f = open('icmp.dat', 'a')
-				f.write(reply.src + ' ')
+				f.write(reply.src + '\n')
 				f.close()
 				x = reply.src + " is online"
 				print(x)
@@ -29,4 +32,69 @@ def icmp_ping(ip1='192.168.1.1', ip2='192.168.1.255'):
 		print("Check the given ips")
 	
 
+def port_identification():
+	lst = list()
+	ip_list = list()
+	port_list = list()
 
+	test = list()
+
+
+
+	for ips in open("../icmp.dat", "r").readlines():
+		lst.append(ips.strip())
+
+	# Validation of the IP adresses.
+	for ip in lst:
+		try:
+			# regexp: [0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$
+			socket.inet_aton(ip) # legal. But need to check if the ip is alive !
+		except Exception as e: # illegal
+			print("Due to the illegal Ip existence, program is exiting. Please check out the ip addresses\
+				in the 'icmp.dat' file.")
+			return
+	
+
+	# Prepearing the nmap
+	def get_nmap(options='-F', ip=ip):
+		command = "nmap " + options + " " + "-oG" + " " + "-" + " " + ip
+		process = os.popen(command)
+		return str(process.read())
+
+
+	a = get_nmap('-F' , 'scanme.nmap.org')
+	a = a.replace("\n", "")
+
+	file = open("nmap_parse.dat", 'w+')
+	file.write(a)
+	file.close()
+
+	# Regex....
+	prt = re.compile(r'(?:.*Ports:\s)(.*)(?:\S\t)(?:Ignored.*)')
+	
+	for line in open('nmap_parse.dat'):
+		ip_list = (prt.findall(line))
+	
+	ip_list = ip_list[0].split(",")
+
+	for item in ip_list:
+		item = item.replace('/'," ")
+		port_list.append(item)
+
+
+	for index, item in enumerate(port_list):
+		item = item.split()
+		port_list[index] = item
+
+	# print(port_list)
+
+	# for iteml, itemp in lst, port_list:
+	# 	item = item + port_list
+	# 	test.append(item)
+
+	wr = [j for i in zip(lst,port_list) for j in i]
+
+	print(flat)
+
+
+port_identification()
